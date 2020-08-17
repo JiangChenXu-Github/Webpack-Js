bd9cda77ac8f717e5e41590ad66c304af05befce
吟阙思町 <68338615@qq.com>
2020-08-17 19:08:47 +0800

    img

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,22 @@ module.exports = {
           'css-loader',
           'less-loader',
         ]
-      }
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // 图片大小小于8kb, 就会被base64处理, 优点是减少请求数量(减轻服务器压力), 缺点是图片体积会更大(文件请求速度更慢)
+          limit: 8 * 1024,
+          //给图片进行重命名, [hash:10]取图片的hash前10位, [ext]取文件原来扩展名
+          name: '[hash:10].[ext]',
+        }
+      },
+      {
+        test: /\.html$/,
+        // 专门处理html文件的img图片(负责引入img, 从而能被url-loader进行处理)
+        loader: 'html-loader'
+      },
     ]
   },
   // plugins的配置(下载->引用->使用)
